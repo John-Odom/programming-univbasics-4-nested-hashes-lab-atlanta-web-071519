221a5c26cea36ed22b3954defbdb9751d043099a
John Odom <joodom91@gmail.com>
2019-06-17 19:09:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,5 +123,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+        
+        programmer_hash:yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        }
 
 end
